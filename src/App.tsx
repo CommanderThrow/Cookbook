410a3e9a509fd1ff66df19526c7bce9516b9e64a
Aaron Ryan <aaronryan0505.ar@gmail.com>
2024-12-16 01:28:49 +0800

    feat: static page and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import recipes from "./data/recipe.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Recipe Finder</h1>
+        <input type="text" placeholder="Search recipes..." disabled />
+        <div>
+          {recipes.map((recipe) => (
+            <div key={recipe.id}>
+              <h2>{recipe.name}</h2>
+              <p>{recipe.instructions}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
