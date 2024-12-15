616c901e8ca26ae74fcad1dccb4976b8a7a42da6
Aaron Ryan <aaronryan0505.ar@gmail.com>
2024-12-16 01:50:53 +0800

    feat: handle dynamic filter and change on search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
 import "./App.css";
+import type React from "react";
+import { useState } from "react";
 import recipes from "./data/recipe.json";
 
 function App() {
+  const [findRecipe, setFindRecipe] = useState("");
+
+  const handleFindRecipe = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setFindRecipe(event.target.value);
+  };
+
+  const filteredRecipes = recipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(findRecipe.toLowerCase())
+  );
+
   return (
     <>
       <div>
         <h1>Recipe Finder</h1>
-        <input type="text" placeholder="Search recipes..." disabled />
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          value={findRecipe}
+          onChange={handleFindRecipe}
+        />
         <div>
-          {recipes.map((recipe) => (
-            <div key={recipe.id}>
-              <h2>{recipe.name}</h2>
-              <img
-                src={recipe.image}
-                alt={recipe.name}
-                style={{ width: "150px", height: "150px", objectFit: "cover" }}
-              />
-              <ul>
-                {recipe.ingredients.map((ingredient) => (
-                  <li key={`${recipe.id}-${ingredient}`}>{ingredient}</li>
-                ))}
-              </ul>
-              <p>{recipe.cooking_time}</p>
-              <p>{recipe.instructions}</p>
-            </div>
-          ))}
+          {filteredRecipes.length > 0 ? (
+            filteredRecipes.map((recipe) => (
+              <div key={recipe.id}>
+                <h2>{recipe.name}</h2>
+                <img
+                  src={recipe.image}
+                  alt={recipe.name}
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    objectFit: "cover",
+                  }}
+                />
+                <ul>
+                  {recipe.ingredients.map((ingredient) => (
+                    <li key={`${recipe.id}-${ingredient}`}>{ingredient}</li>
+                  ))}
+                </ul>
+                <p>Cooking Time: {recipe.cooking_time} minutes</p>
+                <p>{recipe.instructions}</p>
+              </div>
+            ))
+          ) : (
+            <p>No recipes found.</p>
+          )}
         </div>
       </div>
     </>
