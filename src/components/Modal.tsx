82b828750a3ba1f57b21bf07b767840a1a307d34
Aaron Ryan <aaronryan0505.ar@gmail.com>
2024-12-16 15:56:06 +0800

    refactor: add Modal component and destructure the props for Modal and RecipeCard component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,69 @@
+interface Recipe {
+  id: number;
+  name: string;
+  ingredients: string[];
+  cooking_time: number;
+  instructions: string;
+}
+
+interface ModalProps {
+  recipe: Recipe;
+}
+
+const Modal = ({ recipe }: ModalProps) => {
+  const handleModalOpen = (id: number) => {
+    const modal = document.getElementById(
+      `my_modal_${id}`
+    ) as HTMLDialogElement;
+    modal?.showModal();
+  };
+
+  const handleModalClose = (id: number) => {
+    const modal = document.getElementById(
+      `my_modal_${id}`
+    ) as HTMLDialogElement;
+    modal?.close();
+  };
+
+  return (
+    <>
+      <div className="card-actions justify-end">
+        <button
+          type="button"
+          onClick={() => handleModalOpen(recipe.id)}
+          className="btn btn-success"
+        >
+          Detail
+        </button>
+
+        <dialog id={`my_modal_${recipe.id}`} className="modal">
+          <div className="modal-box bg-green-400">
+            <h3 className="font-bold text-3xl pb-3">{recipe.name}</h3>
+            <h2 className="font-bold">Ingredients</h2>
+            <ul>
+              {recipe.ingredients.map((ingredient) => (
+                <li key={ingredient}>{ingredient}</li>
+              ))}
+            </ul>
+            <h2 className="font-bold pt-4">
+              Time: {recipe.cooking_time} minutes
+            </h2>
+            <h2 className="font-bold pt-4">Instructions</h2>
+            <p>{recipe.instructions}</p>
+            <div className="modal-action">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => handleModalClose(recipe.id)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </dialog>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,3 +1,5 @@
+import Modal from "./Modal";
+
 interface Recipe {
   id: number;
   name: string;
@@ -11,73 +13,25 @@ interface RecipeCardProps {
   recipe: Recipe;
 }
 
-const RecipeCard = (recipe: RecipeCardProps) => {
-  const handleModalOpen = (id: number) => {
-    const modal = document.getElementById(
-      `my_modal_${id}`
-    ) as HTMLDialogElement;
-    modal?.showModal();
-  };
-
-  const handleModalClose = (id: number) => {
-    const modal = document.getElementById(
-      `my_modal_${id}`
-    ) as HTMLDialogElement;
-    modal?.close();
-  };
-
+const RecipeCard = ({ recipe }: RecipeCardProps) => {
   return (
     <>
       <div className="card card-side bg-green-400 min-h-[300px] shadow-xl">
         <figure>
-          <img src={recipe.recipe.image} alt={recipe.recipe.name} />
+          <img src={recipe.image} alt={recipe.name} />
         </figure>
         <div className="card-body">
-          <h2 className="text-2xl font-bold">{recipe.recipe.name}</h2>
+          <h2 className="text-2xl font-bold">{recipe.name}</h2>
           <p className="font-bold">
-            Time: {recipe.recipe.cooking_time} minutes
+            Time: {recipe.cooking_time} minutes
           </p>
           <p className="font-bold">Ingredients</p>
           <ul className="m-2 pl-2 list-decimal">
-            {recipe.recipe.ingredients.map((ingredient) => (
-              <li key={`${recipe.recipe.id}-${ingredient}`}>{ingredient}</li>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={`${recipe.id}-${ingredient}`}>{ingredient}</li>
             ))}
           </ul>
-          <div className="card-actions justify-end">
-            <button
-              type="button"
-              onClick={() => handleModalOpen(recipe.recipe.id)}
-              className="btn btn-success"
-            >
-              Detail
-            </button>
-
-            <dialog id={`my_modal_${recipe.recipe.id}`} className="modal">
-              <div className="modal-box bg-green-400">
-                <h3 className="font-bold text-3xl pb-3">
-                  {recipe.recipe.name}
-                </h3>
-                <h2 className="font-bold">Ingredients</h2>
-                <ul>
-                  {recipe.recipe.ingredients.map((ingredient) => (
-                    <li key={ingredient}>{ingredient}</li>
-                  ))}
-                </ul>
-                <h2 className="font-bold pt-4">Time: {recipe.recipe.cooking_time} minutes</h2>
-                <h2 className="font-bold pt-4">Instructions</h2>
-                <p>{recipe.recipe.instructions}</p>
-                <div className="modal-action">
-                  <button
-                    type="button"
-                    className="btn"
-                    onClick={() => handleModalClose(recipe.recipe.id)}
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </dialog>
-          </div>
+          <Modal recipe={recipe} />
         </div>
       </div>
     </>
